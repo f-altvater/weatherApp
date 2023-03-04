a2ecad70a6f160dcf4a78c709b43dbce1dcd1b2d
f-altvater <70957186+f-altvater@users.noreply.github.com>
2023-03-04 16:21:58 +0100

    Delete fetchOptions.js

--- resources/scripts/fetchOptions.js
+++
@@ -1,7 +0,0 @@
-export const geoAPIOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'bbfb1083e8msh70b2d6373cc4e2dp15fd8ejsn349d8f8a9b11',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
-};
